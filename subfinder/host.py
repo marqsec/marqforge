e8d827d81a2cc3570703f1bf0bd94ddfbf5cf202
marqsec <marqlinux@gmail.com>
2026-09-27 02:17:06 +0800

    feat: initialize framework core architecture

--- subfinder/host.py
+++ subfinder/host.py
@@ -1,4 +1,5 @@
 from typing import Dict, Any, Optional, List
+from unittest import result
 from terminal import Commandline
 from configure.cfgtools import Tools
 from urllib.parse import urlparse
@@ -7,5 +8,9 @@ class Checkhost:
     def __init__(self, domain: str = "marqsec.id") -> None:
         self.domain: str = urlparse(domain).netloc
         self.cmd = Commandline()
+        self.listhost_active: List[Dict[str, Any]] = []
+        
+    def check(self):
+        result = self.cmd.execute(Tools("marqsec.id").host)
         
         
